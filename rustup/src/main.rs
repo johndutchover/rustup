673d263b3336dcfec22269a1cbcbe68ff891c291
John Dutchover <johndutchover@users.noreply.github.com>
2025-04-10 23:08:34 -0400

    simple web server

--- rustup/src/main.rs
+++ rustup/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    for stream in listener.incoming() {
+        handle_client(stream.unwrap());
+    }
+}
+fn handle_client(mut stream: TcpStream) {
+    stream.read(&mut [0; 1024]).unwrap();
+    stream.write(b"HTTP/1.1 200 OK\r\n\r\nHello, World!").unwrap();
 }
